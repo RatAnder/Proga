4d00146b81c7f8181e361d2c394724804dd571ce
RatAnder <106764081+RatAnder@users.noreply.github.com>
2022-06-02 19:49:59 +0300

    Add files via upload

--- Stroka.h
+++ Stroka.h
@@ -1,5 +1,21 @@
-#pragma once
+//#pragma once
 #ifndef __STROKA__
 #define __STROKA__
-class{/**/ };
+#include <iostream>
+
+class Stroka
+{
+protected:
+    int len;
+    char* pCh;
+public:
+    Stroka(int = 0);
+    Stroka(char);
+    Stroka(const char*);
+    Stroka(const Stroka&);
+    ~Stroka();
+    char* Getstr(void)const { return pCh; }
+    int GetLen(void)const { return len; }
+    void show();
+};
 #endif // !__STROKA__
